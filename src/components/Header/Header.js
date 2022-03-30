9e51fed70049dd47ed38d4f8dd7b3ad2dd9ab13e
asadulR <asad1252001@gmail.com.com>
2022-03-30 23:41:18 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/Logo.svg';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <Link to='/'><img src={logo} alt="" /></Link>
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Orders</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
