d341e90e98878fb158b2b3922ca426447c935fe7
asadulR <asad1252001@gmail.com.com>
2022-03-31 12:13:57 +0600

    handle Remove Product Btn

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -8,13 +8,22 @@ const Orders = () => {
     // custom hooks
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+
+    const handleRemoveProductBtn = (product) => {
+        console.log(product)
+        const rest = cart.filter(pd => pd.id !== product.id);
+        setCart(rest);
+
+    }
     return (
         <div className='shop-container'>
             <div className="review-items-container">
                 {
                     cart.map(product => <ReviewItem
-                    key={product.id}
-                    product={product}
+                        key={product.id}
+                        product={product}
+
+                        handleRemoveProductBtn = {handleRemoveProductBtn}
                     ></ReviewItem>)
                 }
             </div>
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,7 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
 import React from 'react';
 import './ReviewItem.css'
 const ReviewItem = (props) => {
-    const {name, price, img, shipping, quantity} = props.product;
+    const {handleRemoveProductBtn, product} = props;
+    const {name, price, img, shipping, quantity} = product;
     return (
         <div className='review-item'>
             <div className='review-item-img-container'>
@@ -10,13 +14,15 @@ const ReviewItem = (props) => {
             <div className='review-item-details-container'>
                 <div className="review-item-detail">
                     <h4 title={name}>{name.length > 15 ? name.slice(0, 20) + '...' : name}</h4>
-                    <h4>Price:{price}</h4>
-                    <h4>Shipping: {shipping}</h4>
+                    <h4>Price: ${price}</h4>
+                    <h4>Shipping: ${shipping}</h4>
                     <h4>Quantity: {quantity}</h4>
 
                 </div>
                 <div className="delete-btn-container">
-                    <button>Delete</button>
+                    <button onClick={() => handleRemoveProductBtn(product)}>
+                        <FontAwesomeIcon className='delete-icon' icon={faTrashAlt}></FontAwesomeIcon>
+                    </button>
                 </div>
             </div>
         </div>
