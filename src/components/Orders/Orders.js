865e9f7888c309932625e0be8b9269846c103d19
asadulR <asad1252001@gmail.com.com>
2022-03-31 11:45:02 +0600

    review item added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -2,6 +2,7 @@ import React from 'react';
 import useCart from '../../Hooks/useCart';
 import useProducts from '../../Hooks/useProducts';
 import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
     // custom hooks
@@ -9,8 +10,13 @@ const Orders = () => {
     const [cart, setCart] = useCart(products);
     return (
         <div className='shop-container'>
-            <div className="products-container">
-
+            <div className="review-items-container">
+                {
+                    cart.map(product => <ReviewItem
+                    key={product.id}
+                    product={product}
+                    ></ReviewItem>)
+                }
             </div>
             <div className="cart-container">
                 <Cart cart={cart}></Cart>
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
-const ReviewItem = () => {
+import './ReviewItem.css'
+const ReviewItem = (props) => {
+    const {name, price, img, shipping, quantity} = props.product;
     return (
-        <div>
-            <h2>review items</h2>
+        <div className='review-item'>
+            <div className='review-item-img-container'>
+                <img src={img} alt="" />
+            </div>
+            <div className='review-item-details-container'>
+                <div className="review-item-detail">
+                    <h4 title={name}>{name.length > 15 ? name.slice(0, 20) + '...' : name}</h4>
+                    <h4>Price:{price}</h4>
+                    <h4>Shipping: {shipping}</h4>
+                    <h4>Quantity: {quantity}</h4>
+
+                </div>
+                <div className="delete-btn-container">
+                    <button>Delete</button>
+                </div>
+            </div>
         </div>
     );
 };
